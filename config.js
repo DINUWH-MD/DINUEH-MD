8da2b9ae890bb18bf8389cdf4c1baf2ae71a1245
DINUWH-MD <dinethnethruvan772@gmail.com>
2024-12-23 22:08:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID
-MONGODB: process.env.MONGODB || "enter mongodb here",
+SESSION_ID: process.env.SESSION_ID ||"aZhC0AIa#0i4NC3VxKuYHNo5hA6gLDEKoBxMFB2HfWev5gMYUDUk"
+MONGODB: process.env.MONGODB || "mongodb://mongo:TcJLZkZMuoSQQOrjIbDZYeaqESHaFhCu@mongodb.railway.internal:27017",
 };
