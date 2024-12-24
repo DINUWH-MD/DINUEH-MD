96472994d1063a2540e370bb7a997556cc134da6
DINUWH-MD <dinethnethruvan772@gmail.com>
2024-12-24 21:39:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID ||"eBBG0TyR#mdo4rTIZg0IQg43sWCa2C81mp3rVAZ22GcQ1CL2oEn4"
+SESSION_ID: process.env.SESSION_ID ||"jJ9CzbYD#pEtEl8DdzzaL7gn-8nsOU1QZ8MRBxPmxtT_SVwXqHNI"
 MONGODB: process.env.MONGODB || "mongodb://mongo:TcJLZkZMuoSQQOrjIbDZYeaqESHaFhCu@mongodb.railway.internal:27017",
 };
